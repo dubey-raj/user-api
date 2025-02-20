87ad01c7e5387e46fa669d7e7e372401a97cb7f2
dubey-raj <dubey.rajfgp@gmail.com>
2025-02-20 21:31:48 +0530

    added serilog and global exception handler

--- src/UserService/Program.cs
+++ src/UserService/Program.cs
@@ -1,15 +1,20 @@
 using Amazon.SQS;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
+using Serilog.Formatting.Json;
+using Serilog;
 using UserService.Configurations;
 using UserService.DataStorage.DAL;
 using UserService.Services;
+using UserService.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigureLogging(builder);
 builder.Services.AddControllers();
 builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+AddSwaggerGen(builder);
 builder.Services.AddUsersDBContext(builder.Configuration);
 builder.Services.AddScoped<IUserService, GlamUserService>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
@@ -27,6 +32,8 @@ app.UseCors(policy => policy
 var basePath = "/user-api";
 // Ensure correct path handling in ALB
 app.UsePathBase(new PathString(basePath));
+app.UseSerilogRequestLogging();
+app.UseMiddleware<GlobalExceptionHandler>();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -37,3 +44,47 @@ app.UseRouting();
 app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
+
+static void ConfigureLogging(WebApplicationBuilder builder)
+{
+    //Configure serilog as logger
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console(new JsonFormatter(renderMessage: true))
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+    builder.Host.UseSerilog();
+}
+
+static void AddSwaggerGen(WebApplicationBuilder builder)
+{
+    builder.Services.AddSwaggerGen(c =>
+    {
+        //Add JWT Bearer Authentication to Swagger UI
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Enter 'Bearer <your-token>' below. Example: Bearer eyJhbGciOi..."
+        });
+
+        //Apply Authentication Globally
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+    });
+}
